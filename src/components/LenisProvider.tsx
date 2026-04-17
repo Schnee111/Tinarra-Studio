41772c125924eab225d1505b32bace70361399f9
Muhammad Daffa Ma'arif <muhammaddaffa1357@gmail.com>
2026-04-17 20:17:04 +0700

    refactor: format LenisProvider component for improved readability and maintainability

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -5,7 +5,15 @@ import { ReactLenis } from "lenis/react";
 
 export default function LenisProvider({ children }: { children: ReactNode }) {
   return (
-    <ReactLenis root options={{ smoothWheel: true, duration: 1.5, wheelMultiplier: 0.85 }}>
+    <ReactLenis 
+      root 
+      options={{ 
+        smoothWheel: true, 
+        duration: 1.5, 
+        wheelMultiplier: 0.85,
+        // syncTouch: false (Ini adalah nilai default Lenis. Artinya Lenis tidak akan mengganggu scroll HP)
+      }}
+    >
       {children}
     </ReactLenis>
   );
